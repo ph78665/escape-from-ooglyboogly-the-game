656fb5fe6675502e83b724980beea7acc4bf1cd6
ph78665 <167588182+ph78665@users.noreply.github.com>
2024-04-19 17:34:29 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,298 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image3":
+            case "Eyeball":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 1 1 1 1 1 . . . . . 
+. . . . 1 1 1 1 1 1 1 1 1 . . . 
+. . . 1 1 1 1 1 1 1 1 1 1 1 . . 
+. . . 1 1 1 8 8 8 8 8 1 1 1 . . 
+. . . 1 1 8 8 f f f 8 8 8 1 . . 
+. . . 1 1 8 f f f f f f 8 1 . . 
+. . . 1 1 8 f f f f f f 8 1 1 . 
+. . . 1 1 8 8 f f f 8 8 8 1 1 . 
+. . . b 1 1 1 8 8 8 8 1 1 1 . . 
+. . . . b 1 1 1 1 1 1 1 b b . . 
+. . . . . b 1 1 b b b b . . . . 
+. . . . . . b b . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "Heart":return img`
+. . . 2 2 2 . . . . . . . . . . 
+. . . 2 3 2 . 2 2 2 2 . . . . . 
+. . . 2 3 2 2 2 2 2 3 . . . . . 
+. . 2 2 2 2 2 2 2 2 . . . . . . 
+. 2 2 2 2 2 3 3 3 2 2 . . . . . 
+2 2 2 2 2 2 2 2 3 3 2 2 . . . . 
+3 3 2 2 2 2 2 2 2 3 3 2 . . . . 
+. . . 2 2 2 2 2 2 2 2 2 2 . . . 
+. . . 2 2 2 2 2 2 2 2 2 2 . . . 
+. . . 2 2 2 2 2 2 2 2 2 2 2 . . 
+. . . 2 2 3 3 2 2 2 2 2 3 3 . . 
+. . . . 2 2 3 3 3 2 2 2 2 3 . . 
+. . . . . . 2 2 2 2 2 2 2 3 . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image1":
+            case "OOGLYBOOGLY":return img`
+. . f f f e e e e e e e e e e e 
+. f e e e b f f f f f f f b f f 
+f e f b f d d d d d d d d f . . 
+e e f f d d d d d d f d d f f . 
+e f f d d d d d d d d d d d f . 
+e f d d d f d d d d d d d d f . 
+f f d d d d d d d d d d d d f f 
+. f d d d d d d d d d d d d d f 
+. f d f f d d f d d d d f d d f 
+. f d f b d d f d d d b f d b f 
+. 6 7 f f b b f d d b f f d f f 
+. f 6 7 f f f f d d f f 9 b f . 
+. f f 6 d d 7 6 d d d 9 9 f f . 
+. 7 f 7 f f 7 7 d d d 9 9 f . . 
+. 7 6 7 . f 6 6 6 f f 9 9 . . . 
+. . 6 . . . 6 7 . . . 9 9 . . . 
+`;
+            case "image6":
+            case "Spring":return img`
+. . . . . . b b b . . . . . . . 
+. . . . . b b . b b d d b b . . 
+. . . . . b b . . . b b b . . . 
+. . . . . . b b b b b . b . . . 
+. . . . . . . . . . . . b . . . 
+. . . . . . . . . . . . b . . . 
+. . . . b b b b b b b . b . . . 
+. . . . b . . . . . b b b . . . 
+. . . . b . . . . . d b b . . . 
+. . . . b b b b b d d . b . . . 
+. . . . . . . . . . . . . b . . 
+. . . . . . . . . . . . b b . . 
+. . . . . . . . . . . . b . . . 
+. . . . b . . . . . b b . . . . 
+. . . . b d d d b b b . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image5":
+            case "Grass":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . f . . . . . . 
+. . . . . . . . f f f . . . . . 
+. . . . . . . . f 7 f . . . . . 
+. . . . . . . . f 7 f f f f f . 
+. . . . f f f f 7 7 f f 7 7 f f 
+. . . . f 7 7 f 7 7 f f 7 7 f f 
+. f f f f 7 7 f 7 7 f f 7 7 f f 
+f 7 7 f f 7 7 f 7 7 f f 7 7 7 f 
+. f 7 7 f 7 6 7 7 7 7 f 7 7 7 f 
+. f 7 7 7 7 6 6 7 6 7 f 7 6 7 f 
+. . f 6 6 1 7 6 7 6 6 1 7 6 7 f 
+. . f 7 6 f 6 f f f 6 f 6 6 7 f 
+. . f 7 6 f 6 7 7 1 6 f 6 7 7 f 
+`;
+            case "image7":
+            case "Lemon tree":return img`
+.............6666...............
+..........666667766.6666........
+.........677777777767776........
+......66667775577757777666......
+.....677666675557557776777666...
+.....6776777775555577777766776..
+...66666777777775777777766666...
+.66667767777755757555777776776..
+6666777677775577557555777767766.
+.6667767777777775577777777767666
+.c6566777677777775777777677766..
+cc77666667777777777777777666666c
+cc76666677777777777777775766776c
+c6666776777775777777766677666776
+66667766667776777767767766766666
+ccc76677677776677766767776776ccc
+cc7766776777677677676667767766cc
+.666c676667677766667766666666cc.
+.ccc666766667766666776756665ccc.
+...ccc75c6767666676676677666ccc.
+...cc676c7766676677666666c666cc.
+....c6cc676c6677677c66c665ccc...
+....c5ccc6c66667667cc6ccc6ccc...
+......ccccc66c66566cccccccc.....
+.......cc.cc6c6ccc65ccc.cc......
+...........5ccccccccc...........
+.............feffffe............
+............feefffffe...........
+.........eeeeeffffffee..........
+............fffffef..ee.........
+...............fee..............
+................e...............
+`;
+            case "image8":
+            case "Lemon":return img`
+4 4 4 . . 4 4 4 4 4 . . . . . . 
+4 5 5 4 4 5 5 5 5 5 4 4 . . . . 
+b 4 5 5 1 5 1 1 1 5 5 5 4 . . . 
+. b 5 5 5 5 1 1 5 5 1 1 5 4 . . 
+. b d 5 5 5 5 5 5 5 5 1 1 5 4 . 
+b 4 5 5 5 5 5 5 5 5 5 5 1 5 4 . 
+c d 5 5 5 5 5 5 5 5 4 4 4 4 4 4 
+c d 4 5 5 5 5 5 4 4 d 5 5 5 4 4 
+c 4 5 5 5 d 4 4 5 5 d 5 5 5 4 . 
+c 4 d 5 4 4 d 5 5 5 d 1 5 4 4 . 
+. c 4 4 4 1 d d 5 d d 1 1 4 . . 
+. c 4 5 5 1 1 d d d 5 5 4 4 . . 
+. . c c d d d d 5 5 d d 4 . . . 
+. . . c 4 4 4 4 4 4 4 4 . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image9":
+            case "Pillar":return img`
+.......bbbbbbbbbbbbbbbbbb.......
+......bddddddddddddddddddb......
+.....bddddddddddddddddddddb.....
+....bddddddddddddddddddddddb....
+...bddddddddddddddddddddddddb...
+..bddddddddddddddddddddddddddb..
+..c11111111111111111111111111c..
+..c11111111111111111111111111c..
+.bccccccccccccccccccccccccccccb.
+bb11111dd11dbbbbbbbbd11dd11111bb
+c11bbcc11dd11dbbbbd11dd11ccbb11c
+c1bbddbcb11dd111111dd11bcbddbb1c
+c1bbbddbdbd11dddddd11dbdbddbbb1c
+c11bbddcddbbd111111dbbddcddbb11c
+cb1111dcbd11bbbbbbbb11dbcd1111bc
+.cb111ccbdd1111111111ddbcc111bc.
+..cccc.cbdbb1bb11bb1bbdbc.cccc..
+.......cbdbb1db11bd1bbdbc.......
+.......cbdbd1dd11dd1dbdbc.......
+.......cbdbd1dd11dd1dbdbc.......
+......ccbdbd1dd11dd1dbdbcc......
+.....cbbbdbd1dd11dd1dbdbbbc.....
+.....cdbbdbd1dd11dd1dbdbbdc.....
+.....c11bbbd1dd11dd1dbbb11c.....
+.....cd111bbbbbbbbbbbb111dc.....
+....cccd1111111111111111dccc....
+...cbddbbb111111111111bbbddbc...
+..cbddddddbbbbbbbbbbbbddddddbc..
+..c11111111111111111111111111c..
+..c11111111111111111111111111c..
+..c11111111111111111111111111c..
+..cbbbbbbbbbbbbbbbbbbbbbbbbbbc..
+`;
+            case "image10":
+            case "MAD":return img`
+. . . . 6 6 6 6 6 6 6 . . . . 
+. . 6 6 7 7 7 7 7 7 7 6 6 . . 
+. 6 6 7 7 7 8 8 8 7 7 7 6 6 . 
+. 6 7 7 7 8 8 7 8 8 7 7 7 6 . 
+. c 7 7 8 8 8 8 8 8 8 7 7 c . 
+. c 9 7 8 7 7 7 7 7 8 7 9 c . 
+. c 9 9 7 7 7 7 7 7 7 9 9 c . 
+. c 6 6 9 9 9 9 9 9 9 6 6 c . 
+c c 6 6 6 6 6 6 6 6 6 6 6 c c 
+c d c c 6 6 6 6 6 6 6 c c d c 
+c d d d c c c c c c c d d d c 
+c c b d d d d d d d d d b c c 
+c c c c c b b b b b c c c c c 
+c c b b b b b b b b b b b c c 
+. c c b b b b b b b b b c c . 
+. . . c c c c c c c c c . . . 
+`;
+            case "image2":
+            case "Scare":return img`
+ddddddddddddddddffdffddddddddddd
+dddddddddddddddddfdfdddddddddddd
+dddd777777777dddfdddfddddddddddd
+dddd77776677777ddddddddddddddddd
+dddf77766666777dddddddddffffdddd
+dddf76777776777ddddddddfffffffdd
+dddf76677777777ddddddffff77fffdd
+dddf7767777767fdddddf77777777ffd
+ddd77677776667fddddff77777777ffd
+d77776766777777ddddff76777777ffd
+d77766766776777ddddff76777767ffd
+d77767776776677dddddf76677767ffd
+dd7777776676677dddddff7677767fdd
+dddf77777777777ddddddf767776ffdd
+dddfff777777777ddddddd777766dddd
+dddddddddd777ddddddddd777777dddd
+ddddddddddddddddddddd7766777dddd
+ddddddddddddddddddddd777667ddddd
+dddddddddddddddddddddd77777ddddd
+dddddddddddddddddddddddddddddddd
+dddddddddddddddddddddddddddddddd
+ddddddddddddddffffffffdddddddddd
+ddddddddddddfffddddddffffddddddd
+ddddddddddfffdddddddddddffdddddd
+ddddddddffdddddddddddddddffddddd
+dddddddffddddddddddddddddddfdddd
+dddddfffdddddddddddddddddddffddd
+ddddffddddddddddddddddddddddffdd
+dddffddddddddddddddddddddddddfdd
+dddfdddddddddddddddddddddddddddd
+dddddddddddddddddddddddddddddddd
+dddddddddddddddddddddddddddddddd
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Boogers":
+            case "anim1":return [img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`, img`
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d f f f f f f f f d d d d d 
+d d f f 7 7 7 7 7 7 f f d d d d 
+d f f f f 7 7 6 7 7 f f f d d d 
+d f 7 f f f 7 7 7 f f f f d d d 
+d f 7 7 f 7 7 f f f 7 7 f f d d 
+d f 7 7 7 7 7 f 7 f 7 7 f f d d 
+d f 7 7 7 6 7 f f f 7 7 f f d d 
+d d f 7 f 7 6 7 7 7 f 7 f f d d 
+d d f f 7 7 7 7 7 6 7 7 f f d d 
+d d d f f f f 6 7 f f f f f d d 
+d d d d d f 7 7 7 f f f f d d d 
+d d d d d d 7 7 7 d d d d d d d 
+d d d d d d 7 7 d d d d d d d d 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
